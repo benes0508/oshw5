f09c8bd9d3da315b401450fc665c3d331f54b755
ben <bene@mail.tau.ac.il>
2024-03-19 22:15:36 +0200

    Sdfg

--- tmr/pcc_server.c
+++ tmr/pcc_server.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 #include <signal.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -11,30 +11,19 @@
 #define PRINTABLE_CHAR_MAX 126
 
 unsigned short pcc_total[PRINTABLE_CHAR_MAX - PRINTABLE_CHAR_MIN + 1] = {0};
-volatile sig_atomic_t processing_client = 0;
 volatile sig_atomic_t sigint_received = 0;
 
-void handle_sigint(int sig_num) {
-    if (processing_client == 0) {
-        // If not processing a client, print statistics and exit.
-        for (int i = PRINTABLE_CHAR_MIN; i <= PRINTABLE_CHAR_MAX; i++) {
-            if (pcc_total[i - PRINTABLE_CHAR_MIN] > 0) {
-                printf("char '%c' : %hu times\n", i, pcc_total[i - PRINTABLE_CHAR_MIN]);
-            }
-        }
-        exit(0);
-    } else {
-        // If processing a client, set flag to handle SIGINT after processing is done.
-        sigint_received = 1;
-    }
+void sigint_handler(int sig) {
+    sigint_received = 1;
 }
 
-void print_statistics() {
+void print_statistics_and_exit() {
     for (int i = PRINTABLE_CHAR_MIN; i <= PRINTABLE_CHAR_MAX; i++) {
         if (pcc_total[i - PRINTABLE_CHAR_MIN] > 0) {
             printf("char '%c' : %hu times\n", i, pcc_total[i - PRINTABLE_CHAR_MIN]);
         }
     }
+    exit(0);
 }
 
 int main(int argc, char *argv[]) {
@@ -43,20 +32,16 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    struct sigaction sa;
-    memset(&sa, 0, sizeof(sa));
-    sa.sa_handler = &handle_sigint;
-    sigaction(SIGINT, &sa, NULL);
-
-    int server_fd, new_socket;
+    int server_fd, new_socket, port;
     struct sockaddr_in address;
     int opt = 1;
     socklen_t addrlen = sizeof(address);
-    int port = atoi(argv[1]);
+
+    signal(SIGINT, sigint_handler);
 
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == 0) {
-        perror("socket failed");
+    if (server_fd < 0) {
+        perror("Socket creation error");
         exit(EXIT_FAILURE);
     }
 
@@ -65,61 +50,47 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
+    port = atoi(argv[1]);
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(port);
 
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
+        perror("Bind failed");
         exit(EXIT_FAILURE);
     }
 
     if (listen(server_fd, 10) < 0) {
-        perror("listen");
+        perror("Listen");
         exit(EXIT_FAILURE);
     }
 
-    while (!sigint_received) {
+    while (1) {
+        if (sigint_received) {
+            print_statistics_and_exit();
+        }
+
         new_socket = accept(server_fd, (struct sockaddr *)&address, &addrlen);
         if (new_socket < 0) {
             if (errno == EINTR && sigint_received) {
-                break;
+                print_statistics_and_exit();
             }
-            perror("accept");
             continue;
         }
 
-        uint16_t n_bytes = 0, c_bytes = 0;
-        ssize_t bytes_read = read(new_socket, &n_bytes, sizeof(n_bytes));
-        if (bytes_read <= 0) {
-            perror("read from client");
-            close(new_socket);
-            continue;
-        }
-
+        uint16_t n_bytes, c_bytes = 0;
+        read(new_socket, &n_bytes, sizeof(n_bytes));
         n_bytes = ntohs(n_bytes);
-        unsigned short received_chars[PRINTABLE_CHAR_MAX - PRINTABLE_CHAR_MIN + 1] = {0};
-        char buffer[1024];
-        int full_data_received = 1;
 
-        while (n_bytes > 0) {
-            bytes_read = read(new_socket, buffer, sizeof(buffer));
-            if (bytes_read <= 0) {
-                full_data_received = 0;
-                break;
-            }
-            n_bytes -= bytes_read;
+        char buffer[1024];
+        ssize_t bytes_read;
+
+        while ((bytes_read = read(new_socket, buffer, sizeof(buffer))) > 0) {
             for (int i = 0; i < bytes_read; ++i) {
                 if (buffer[i] >= PRINTABLE_CHAR_MIN && buffer[i] <= PRINTABLE_CHAR_MAX) {
                     c_bytes++;
-                    received_chars[buffer[i] - PRINTABLE_CHAR_MIN]++;
+                    pcc_total[buffer[i] - PRINTABLE_CHAR_MIN]++;
                 }
-            }
-        }
-
-        if (full_data_received) {
-            for (int i = 0; i < PRINTABLE_CHAR_MAX - PRINTABLE_CHAR_MIN + 1; i++) {
-                pcc_total[i] += received_chars[i];
             }
         }
 
@@ -128,7 +99,5 @@ int main(int argc, char *argv[]) {
         close(new_socket);
     }
 
-    print_statistics();
-    close(server_fd);
     return 0;
 }
