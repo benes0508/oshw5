767c01885a71a1ed3fcb32da1a7dca9755cd9571
ben <bene@mail.tau.ac.il>
2024-03-19 21:55:11 +0200

    new server1

--- tmr/pcc_server.c
+++ tmr/pcc_server.c
@@ -5,20 +5,22 @@
 #include <signal.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 #include <errno.h>
 
+#define PORT 12345 // This should be replaced with argv[1]
+#define PRINTABLE_CHAR_MIN 32
+#define PRINTABLE_CHAR_MAX 126
 
-#define MAX_PRINTABLE 126
-#define MIN_PRINTABLE 32
-#define PRINTABLE_RANGE (MAX_PRINTABLE - MIN_PRINTABLE + 1)
-
-volatile sig_atomic_t stop_server = 0;
-
-unsigned short pcc_total[PRINTABLE_RANGE] = {0};
+unsigned short pcc_total[PRINTABLE_CHAR_MAX - PRINTABLE_CHAR_MIN + 1] = {0};
 
 void sigint_handler(int sig) {
-    stop_server = 1;
+    // Ensure this handler does not interrupt a client being processed by making it atomic if necessary
+    for (int i = PRINTABLE_CHAR_MIN; i <= PRINTABLE_CHAR_MAX; i++) {
+        if (pcc_total[i - PRINTABLE_CHAR_MIN] > 0) {
+            printf("char '%c' : %hu times\n", i, pcc_total[i - PRINTABLE_CHAR_MIN]);
+        }
+    }
+    exit(0);
 }
 
 int main(int argc, char *argv[]) {
@@ -27,24 +29,34 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    signal(SIGINT, sigint_handler);
+    unsigned int port = atoi(argv[1]);
+    if (port == 0 || port > 65535) {
+        fprintf(stderr, "Invalid port number\n");
+        return 1;
+    }
 
-    int server_fd, new_socket, valread;
+    int server_fd, new_socket;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
-    unsigned int port = atoi(argv[1]);
 
+    // Register SIGINT handler
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = sigint_handler;
+    sigaction(SIGINT, &sa, NULL);
+
+    // Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+    // Forcefully attaching socket to the port
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
-
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(port);
@@ -58,16 +70,15 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    while (!stop_server) {
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
-            if (errno == EINTR) {
-                continue;
-            }
+    //printf("Server is listening on port %d...\n", port);
+
+    while (1) {
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
             perror("accept");
             continue;
         }
 
-        uint16_t n_bytes, char_count = 0;
+        uint16_t n_bytes, c_bytes = 0;
         if (read(new_socket, &n_bytes, sizeof(n_bytes)) <= 0) {
             perror("read");
             close(new_socket);
@@ -75,14 +86,14 @@ int main(int argc, char *argv[]) {
         }
 
         n_bytes = ntohs(n_bytes);
-        char buffer[1024];
+        char buffer[1025]; // Considering a null-terminator for safe string operations if needed
         ssize_t bytes_read;
-        while (n_bytes > 0 && (bytes_read = read(new_socket, buffer, sizeof(buffer))) > 0) {
+        while (n_bytes > 0 && (bytes_read = read(new_socket, buffer, sizeof(buffer) - 1)) > 0) {
             n_bytes -= bytes_read;
             for (int i = 0; i < bytes_read; ++i) {
-                if (buffer[i] >= MIN_PRINTABLE && buffer[i] <= MAX_PRINTABLE) {
-                    char_count++;
-                    pcc_total[buffer[i] - MIN_PRINTABLE]++;
+                if (buffer[i] >= PRINTABLE_CHAR_MIN && buffer[i] <= PRINTABLE_CHAR_MAX) {
+                    c_bytes++;
+                    pcc_total[buffer[i] - PRINTABLE_CHAR_MIN]++;
                 }
             }
         }
@@ -93,19 +104,13 @@ int main(int argc, char *argv[]) {
             continue;
         }
 
-        char_count = htons(char_count);
-        if (write(new_socket, &char_count, sizeof(char_count)) < 0) {
+        c_bytes = htons(c_bytes);
+        if (write(new_socket, &c_bytes, sizeof(c_bytes)) < 0) {
             perror("write");
         }
+
         close(new_socket);
     }
 
-    for (int i = 0; i < PRINTABLE_RANGE; ++i) {
-        if (pcc_total[i] > 0) {
-            printf("char '%c' : %hu times\n", i + MIN_PRINTABLE, pcc_total[i]);
-        }
-    }
-
-    close(server_fd);
     return 0;
 }
